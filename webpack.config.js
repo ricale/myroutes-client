48feb13d1d8e2e16ee7a17b41dcf1115d3617d18
ricale <KIM.kangseong@gmail.com>
2017-10-23 10:38:50 +0900

    Initial commit

---
+++ webpack.config.js
@@ -0,0 +1,61 @@
+const path = require('path');
+const webpack = require('webpack');
+const ProgressBarPlugin = require('progress-bar-webpack-plugin');
+
+module.exports = {
+  context: path.resolve(__dirname, './'),
+  entry: {
+    index: './src/index.jsx'
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/dist'
+  },
+  devServer: {
+  },
+  devtool: "cheap-eval-source-map",
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  plugins: [
+    new ProgressBarPlugin()
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        use: [{
+          loader: 'babel-loader',
+          options: {presets: ['react', 'es2015', 'stage-0']}
+        }],
+        exclude: [/node_moduels/]
+      },
+      {
+        test: /\.less$/,
+        use: ['style-loader', 'css-loader', 'less-loader']
+      },
+
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: "url-loader?limit=10000&mimetype=application/font-woff&name=fonts/[hash].[ext]"
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=application/octet-stream&name=fonts/[hash].[ext]"
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=65000&mimetype=application/octet-stream&name=fonts/[hash].[ext]"
+      },
+      {
+        test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+        loader: "url-loader?limit=10000&mimetype=image/svg+xml&name=fonts/[hash].[ext]"
+      },
+    ]
+  }
+}
